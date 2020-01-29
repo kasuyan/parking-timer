f211d4284072b9593015af2c47be51bfe8bb5f71
kasuya_kazunori <kasuya_kazunori@cyberagent.co.jp>
2020-01-29 15:50:32 +0900

    added: home screen page

--- src/main.js
+++ src/main.js
@@ -6,3 +6,8 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js")
+  .catch(() => {})
+}
